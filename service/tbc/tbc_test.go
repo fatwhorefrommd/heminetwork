aa2ab0b1f00dcd5f21224fe0d59f589a728c5ea6
AL-CT <antoniomartins@protonmail.com>
2025-04-03 11:33:16 +0000

    test2

--- service/tbc/tbc_test.go
+++ service/tbc/tbc_test.go
@@ -137,7 +137,7 @@ func TestDbUpgradePipeline(t *testing.T) {
 	// 	t.Fatal(err)
 	// }
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer func() {
 		cancel()
 	}()
